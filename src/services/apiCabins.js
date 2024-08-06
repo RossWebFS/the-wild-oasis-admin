bc4d7ab35cdf605dace68fbce136ef14f2062824
RossWebFS <shimon.vip2006@gmail.com>
2024-08-06 19:32:24 +0300

    Added fetching and deleting cabins

---
+++ src/services/apiCabins.js
@@ -0,0 +1,21 @@
+import supabase from "./supabase";
+
+export const getCabins = async () => {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return data;
+};
+
+export const deleteCabin = async (id) => {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    throw new Error(error);
+  }
+
+  return data;
+};
