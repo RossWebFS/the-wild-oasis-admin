6151c8b333d6c14497ddeb86d10082ec4c60282e
RossWebFS <shimon.vip2006@gmail.com>
2024-08-13 13:37:47 +0300

    Added pagination prefetching

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 
 import { getBookings } from "../../services/apiBookings";
+import { PAGE_SIZE } from "../../data/constants";
 
 export const useBookings = () => {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   const filterValue = searchParams.get("status");
@@ -26,6 +28,22 @@ export const useBookings = () => {
     queryKey: ["bookings", filter, sortBy, currentPage],
     queryFn: () => getBookings({ filter, sortBy, currentPage }),
   });
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (currentPage < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, currentPage + 1],
+      queryFn: () =>
+        getBookings({ filter, sortBy, currentPage: currentPage + 1 }),
+    });
+
+  if (currentPage > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, currentPage - 1],
+      queryFn: () =>
+        getBookings({ filter, sortBy, currentPage: currentPage - 1 }),
+    });
 
   return { bookings, isLoading, error, count };
 };
