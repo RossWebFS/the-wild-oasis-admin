9ba88bb76ceb9ae52d10e0ba8fef19455f3d22ce
RossWebFS <shimon.vip2006@gmail.com>
2024-08-15 22:37:02 +0300

    Added protected route for authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 
 import { login as loginAPI } from "../../services/apiAuth";
 
 export const useLogin = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueriesData(["user", user]);
       navigate("/");
     },
     onError: () => {
