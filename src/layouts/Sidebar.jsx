f579706551f736ddd4594e7d12943cac5473f394
RossWebFS <shimon.vip2006@gmail.com>
2024-08-18 09:40:43 +0300

    Fixed small errors

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -2,15 +2,12 @@ import styled from "styled-components";
 
 import Logo from "../ui/Logo";
 import MainNav from "../ui/MainNav";
-import Uploader from "../data/Uploader";
 
 const Sidebar = () => {
   return (
     <StyledSidebar>
       <Logo />
       <MainNav />
-
-      <Uploader />
     </StyledSidebar>
   );
 };
